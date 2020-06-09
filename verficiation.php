3a6db663a03ead252ad9057737eef39c674a3cb1
kicbar <bartlomiejkicior@gmail.com>
2020-06-09 22:05:38 +0200

    check connection to database

--- verficiation.php
+++ verficiation.php
@@ -11,7 +11,13 @@
 		}
         else 	
     	{	
-            echo 'connected'; 
+            echo 'connected</br>'; 
+            $sql_check_connection = "select fname from clients where pesel = '95012309090' ";
+            $sql_check_connection_compiled = oci_parse($connection, $sql_check_connection);
+            oci_execute($sql_check_connection_compiled); 
+            $get_fname = oci_fetch_object($sql_check_connection_compiled);
+            $fname_client = $get_fname->FNAME;
+            echo 'Fname from database is: '.$fname_client;
             oci_close($connection);
         }
 	}
@@ -21,5 +27,4 @@
 		echo '<span style="color: red">Błąd łączenia z serwerem.</span>';
 		echo '<br/> For dev info: '.$error;
 	}
-
 ?>
